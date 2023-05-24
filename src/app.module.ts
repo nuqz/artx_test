1441dc6893fadc36a4c038b3ac5d214d58e9cfe4
Nikolay Koshenkov <23371089+nuqz@users.noreply.github.com>
2023-05-24 14:25:12 +0300

    Simple PostgreSQL config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'insecure_password',
+      database: 'postgres',
+      entities: [],
+      synchronize: true, // WARN: Do not use in production!
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
